1203fba5ecd3ce9b3f7ebc19e24edea8cbaf144c
Trisha2910tinaaaaa <trishasonii.29@gmail.com>
2026-08-21 11:26:11 +0530

    refactor:optimize pdf extractor

--- src/pdf_extractor.py
+++ src/pdf_extractor.py
@@ -1,18 +1,27 @@
 import fitz
+from .ocr import ocr_page
 
-def extract_text_from_pdf(pdf_path):
+
+def extract_pdf(pdf_path, ocr_threshold=50):
 
     doc = fitz.open(pdf_path)
 
     pages = []
 
-    for page_number, page in enumerate(doc):
+    for page_number, page in enumerate(doc, start=1):
 
-        text = page.get_text()
+        text = page.get_text().strip()
+
+        extraction_method = "text"
+
+        if len(text) < ocr_threshold:
+            text = ocr_page(page)
+            extraction_method = "ocr"
 
         pages.append({
-            "page_number": page_number + 1,
-            "text": text
+            "page_number": page_number,
+            "text": text,
+            "extraction_method": extraction_method
         })
 
     return pages
