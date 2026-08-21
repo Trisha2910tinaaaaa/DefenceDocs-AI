132b179dd28b58018725a786e0ac2c76b47ce177
Trisha2910tinaaaaa <trishasonii.29@gmail.com>
2026-08-21 12:16:32 +0530

    refactor:added SEMANTIC SEARCH functionality

--- src/vector_store.py
+++ src/vector_store.py
@@ -16,3 +16,34 @@ def create_index(embeddings):
     index.add(embeddings)
 
     return index
+def search(
+    index,
+    model,
+    chunks,
+    query,
+    top_k=5
+):
+
+    query_embedding = model.encode(
+        [query]
+    ).astype("float32")
+
+    distances, indices = index.search(
+        query_embedding,
+        top_k
+    )
+
+    results = []
+
+    for distance, idx in zip(
+        distances[0],
+        indices[0]
+    ):
+
+        result = chunks[idx].copy()
+
+        result["distance"] = float(distance)
+
+        results.append(result)
+
+    return results
